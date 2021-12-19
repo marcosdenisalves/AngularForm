a2977e04faaf75b3de8949bc980e7f07e31b36c9
marcosdenisalves <dee.alves2@gmail.com>
2021-12-19 05:28:30 -0300

    Applying style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { ListTableComponent } from './list-table/list-table.component';
-import { Validators, FormBuilder } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { AppService } from './app.service';
 import { MatDialog } from '@angular/material/dialog';
 import { Component} from '@angular/core';
 import { Notary } from './model/notary';
+import { MatTableDataSource } from '@angular/material/table';
+import { Certificate } from './model/certificate';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Sort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +15,15 @@ import { Notary } from './model/notary';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  dataSource = new MatTableDataSource<Certificate>();
+  certificates: Array<Certificate> = [];
+
+  displayedColumns: string[] = ['id', 'name'];
+
   notary: Notary = new Notary('', '', '', '', '', '', null);
 
   constructor(
+    private _liveAnnouncer: LiveAnnouncer,
     private service: AppService,
     private dialog: MatDialog,
     private fb: FormBuilder
@@ -34,11 +44,21 @@ export class AppComponent {
       width: '100%',
     }).afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
+      this.certificates = result;
+      this.dataSource.data = result;
     });
   }
 
   onSubmit() {
     this.service.insert(this.notary).subscribe();
     console.warn(this.notaryGroup.value);
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
